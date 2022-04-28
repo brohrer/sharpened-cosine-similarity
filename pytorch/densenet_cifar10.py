f3ed3e0998ba96c543213e3b5c773461bba435de
Stephen <st624@cornell.edu>
2022-04-28 18:49:40 -0400

    fixed brackets in seed function

--- pytorch/densenet_cifar10.py
+++ pytorch/densenet_cifar10.py
@@ -25,7 +25,7 @@ import torch
 
 def set_all_seeds(seed):
   random.seed(seed)
-  os.environ('PYTHONHASHSEED') = str(seed)
+  os.environ['PYTHONHASHSEED'] = str(seed)
   np.random.seed(seed)
   torch.manual_seed(seed)
   torch.cuda.manual_seed(seed)
