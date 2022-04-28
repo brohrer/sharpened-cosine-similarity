831fab5a960b867d411aa14a109a83cf7d659451
Stephen <st624@cornell.edu>
2022-04-28 18:37:00 -0400

    removed original network

--- pytorch/densenet_cifar10.py
+++ pytorch/densenet_cifar10.py
@@ -65,59 +65,6 @@ testing_loader = DataLoader(
     batch_size=batch_size,
     shuffle=False)
 
-
-class Network(nn.Module):
-    def __init__(self):
-        super().__init__()
-
-        self.scs1 = SharpenedCosineSimilarity(
-            in_channels=n_input_channels,
-            out_channels=n_units_1,
-            kernel_size=5,
-            padding=0)
-        self.pool1 = MaxAbsPool2d(kernel_size=2, stride=2, ceil_mode=True)
-
-        self.scs2 = SharpenedCosineSimilarity(
-            in_channels=n_units_1,
-            out_channels=n_units_2,
-            kernel_size=5,
-            padding=1)
-        self.pool2 = MaxAbsPool2d(kernel_size=2, stride=2, ceil_mode=True)
-
-        self.scs3 = SharpenedCosineSimilarity(
-            in_channels=n_units_2,
-            out_channels=n_units_3,
-            kernel_size=5,
-            padding=1)
-        self.pool3 = MaxAbsPool2d(kernel_size=4, stride=4, ceil_mode=True)
-        self.out = nn.Linear(in_features=n_units_3, out_features=n_classes)
-
-    def n_params(self):
-        n = 0
-        for scs in [self.scs1, self.scs2, self.scs3]:
-            n += (
-                np.prod(scs.weight.shape) +
-                np.prod(scs.p.shape) +
-                np.prod(scs.q.shape))
-        n += np.prod(self.out.weight.shape)
-        return n
-
-    def forward(self, t):
-        t = self.scs1(t)
-        t = self.pool1(t)
-
-        t = self.scs2(t)
-        t = self.pool2(t)
-
-        t = self.scs3(t)
-        t = self.pool3(t)
-
-        t = t.reshape(-1, n_units_3)
-        t = self.out(t)
-
-        return t
-
-
 # Restore any previously generated results.
 try:
     accuracy_results = np.load(accuracy_results_path).tolist()
